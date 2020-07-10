d7b3f24104b5192185478fd8dc4a2a64bfc83c90
Bryan Fields <bryan@bryanfields.net>
2020-07-10 21:17:17 +0000

    updates to the error handler

--- notice.sh
+++ notice.sh
@@ -10,6 +10,84 @@ TMP_FILE="/tmp/db-query-notice.txt"
 #default for NODUPDATE
 NOUPDATE='0'
 
+function Query_DB {
+#Function to query the DB
+
+Query="SELECT Latitude, Longitude, antenna_Height_Meters , ERP,  Output_frequency, 
+  REPLACE(Repeater_city, ' ','-') AS City, Repeater_callsign,  emission_1, emission_2, 
+  COORDINATED, chan_Size_kHz, record_ID, Service_Ring_km, Interference_Ring_km, adj1_ring_km, adj2_ring_km, modeled, model_Required, model_Name,
+  CTCSS_IN, CTCSS_OUT, DCS_CODE, Holder_name, Trustee_name, Trustee_email_adddress, County, Repeater_city
+  FROM filemaker  WHERE record_ID = '${RECORD_ID}';"
+ #       echo "$Query"
+        mysql import -e "$Query" -NB | tr '\t' '|' >"$TMP_FILE"
+#cat $TMP_FILE
+}
+
+function READ_FILE {
+while IFS='|' read -a array
+do 
+
+#0 Lat | 1 Long | 2 AGL | 3 ERP | 4 Freq | 5 City | 6 call | 7 em1 | 8 em2 | 9 COORDINATED | 10 chan_Size | 11 record_ID
+#12 Service_Ring_km | 13 Interference_Ring_km | 14 adj1_ring_km | 15 adj2_ring_km | 16 modeled | 17 model_Required | 18  model_Name
+#19 CTCSS_IN | 20 CTCSS_OUT | 21 DCS_CODE | 22 Holder | 23 Trustee | 24 Trustee Email | 25 County | 26 City
+LAT="${array[0]}"
+LON="${array[1]}"
+AGL="${array[2]}"
+ERP="${array[3]}"
+FREQ="${array[4]}" 
+CITY="${array[5]}"
+CALL="${array[6]}"
+EM1="${array[7]}"
+EM2="${array[8]}"
+if [[ ${array[9]} = 1 ]]
+	then
+	COORD="COORDINATED"
+	else
+	COORD="UNCOORDINATED"
+fi
+CHAN_SIZE="${array[10]}"
+ID="${array[11]}"
+SVC_RING="${array[12]}"
+INT_RING="${array[13]}"
+ADJ1_RING="${array[14]}"
+ADJ2_RING="${array[15]}"
+MODELED="${array[16]}"
+MOD_REQ="${array[17]}"
+MODEL_URL="${array[18]}"
+CTCSS_IN="${array[19]}"
+CTCSS_OUT="${array[20]}"
+DCS_CODE="${array[21]}"
+HOLDER="${array[22]}"
+TRUSTEE="${array[23]}"
+TRUSTEE_EMAIL="${array[24]}"
+REPEATER_COUNTY="${array[25]}"
+REPEATER_CITY="${array[26]}"
+done < "$TMP_FILE"
+# rm "$TMP_FILE"
+
+}
+
+function COMPOSE_EMAIL {
+
+if [ -z ${TRUSTEE_EMAIL} ] || [ ${TRUSTEE_EMAIL} = NULL ]
+then
+	echo "ERROR: Record: ${ID} TRUSTEE_EMAIL is NULL or Empty"
+	exit 255
+fi  
+
+while IFS= read -r line
+do
+	while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
+        LHS=${BASH_REMATCH[1]}
+        RHS="$(eval echo "\"$LHS\"")"
+        line=${line//$LHS/$RHS}
+    	done
+    echo "$line"
+done < "$TEXT_FILE"
+}
+
+
+
 # Options 
 # -id database ID
 # -model even if modeled flag is set in db
@@ -28,7 +106,7 @@ while [[ $# -gt 0 ]]
 		shift
 		;;
 		-input) #input file name
-		INPUT_FILE="$2"
+		TEXT_FILE="$2"
 		shift
 		;;
 		-noupdate)
@@ -63,103 +141,20 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 #Below is run to match the DB
 
-if [[ ${SCANDB} = '1' ]]
-	then
-		echo "SCANNING DATABASE FOR CHANGES"
-	  Query="SELECT Latitude, Longitude, antenna_Height_Meters , ERP,  Output_frequency, 
-		REPLACE(Repeater_city, ' ','-') AS City, Repeater_callsign,  emission_1, emission_2, 
-		COORDINATED, chan_Size_kHz, record_ID, Service_Ring_km, Interference_Ring_km, adj1_ring_km, adj2_ring_km, modeled, model_Required, model_Name
-		FROM filemaker  WHERE model_Required = '1' ;"
-	  echo "$Query"
-	  mysql import -e "$Query" -NB | tr '\t' '|' >"$TMP_FILE" 
-fi
-
-if [[ ${MODELED} = '1' ]]
-	then
-		echo "ignore modeled ones"
-		modeled=" AND modeled = '0'"
-fi
-
-if [[ ! -z ${FREQ_LOW} && ! -z ${FREQ_HIGH} ]] 
-then
-	echo FREQ_LOW \"${FREQ_LOW}\"
-	echo FREQ_HIGH \"${FREQ_HIGH}\"
-	Query="SELECT Latitude, Longitude, antenna_Height_Meters , ERP,  Output_frequency, 
-  REPLACE(Repeater_city, ' ','-') AS City, Repeater_callsign,  emission_1, emission_2, 
-  COORDINATED, chan_Size_kHz, record_ID, Service_Ring_km, Interference_Ring_km, adj1_ring_km, adj2_ring_km, modeled, model_Required, model_Name
-  FROM filemaker  WHERE Output_frequency BETWEEN '${FREQ_LOW}' AND '${FREQ_HIGH}' AND COORDINATED = '1'  ${modeled}
-  ORDER BY  Output_frequency ;"
-	echo "$Query"
-	mysql import -e "$Query" -NB | tr '\t' '|' >"$TMP_FILE"
-#	cat $TMP_FILE
-elif [[ ! -z ${FREQ_LOW+x} || ! -z ${FREQ_HIGH+x} ]]
-then 
-	echo FREQ_LOW and FREQ_HIGH must be set
-fi
-
 # model only one record ID
 
 if [[ ! -z ${RECORD_ID} ]]
-then
-	Query="SELECT Latitude, Longitude, antenna_Height_Meters , ERP,  Output_frequency, 
-  REPLACE(Repeater_city, ' ','-') AS City, Repeater_callsign,  emission_1, emission_2, 
-  COORDINATED, chan_Size_kHz, record_ID, Service_Ring_km, Interference_Ring_km, adj1_ring_km, adj2_ring_km, modeled, model_Required, model_Name
-  FROM filemaker  WHERE record_ID = '${RECORD_ID}';"
-	echo "$Query"
-	mysql import -e "$Query" -NB | tr '\t' '|' >"$TMP_FILE"
-fi
-
-#Take an input file and model it
-#will this overwrite another file?  idk
-if [[ ! -z ${INPUT_FILE} ]]
-then
-	TMP_FILE="${INPUT_FILE}"
-fi
-
-
-#this is the main loop for the program and the coordination logic
-
-while IFS='|' read -a array
-do 
-
-#0 Lat | 1 Long | 2 AGL | 3 ERP | 4 Freq | 5 City | 6 call | 7 em1 | 8 em2 | 9 COORDINATED | 10 chan_Size | 11 record_ID
-#12 Service_Ring_km | 13 Interference_Ring_km | 14 adj1_ring_km | 15 adj2_ring_km | 16 modeled | 17 model_Required | 18  model_Name
-LAT="${array[0]}"
-LON="${array[1]}"
-TXH="${array[2]}"
-ERP="${array[3]}"
-FREQ="${array[4]}" 
-CITY="${array[5]}"
-CALL="${array[6]}"
-EM1="${array[7]}"
-EM2="${array[8]}"
-if [[ ${array[9]} = 1 ]]
 	then
-	COORD="COORDINATED"
-	else
-	COORD="UNCOORDINATED"
+	Query_DB
 fi
-CHAN_SIZE="${array[10]}"
-ID="${array[11]}"
-SVC_RING="${array[12]}"
-INT_RING="${array[13]}"
-ADJ1_RING="${array[14]}"
-ADJ2_RING="${array[15]}"
-MODELED="${array[16]}"
-MOD_REQ="${array[17]}"
-MOD_NAME="${array[18]}"
-
-	else echo frequency ${array[4]} of record number ${array[4]} is out of bounds
+if [[ -z ${TEXT_FILE} ]]
+	then
+	echo "ERROR: -input missing template file name"
+	exit 255
 fi
-done < "$TMP_FILE"
-# rm "$TMP_FILE"
 
 
 
-#
-# TODO: not sure where the VARs in func. INTERFERENCE are coming from
-#       or how namespaces work in bash or that you could have function calls in bash
-#
 
-
-exit
+READ_FILE
+COMPOSE_EMAIL
